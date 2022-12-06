f0870c31a828e10c163dfe45aae37861b688016a
Niklas Rasmusson <niklas@neatminds.se>
2022-12-06 10:34:25 +0100

    Day 4 step 2

--- day-04/main.py
+++ day-04/main.py
@@ -15,7 +15,7 @@ def irange(a: int, b: int) -> 'range':
     return range(a, b + 1)
 
 
-def step_1():
+def step_1() -> int:
     text = read_input()
     pairs = parse_input(text)
     result = 0
@@ -23,10 +23,27 @@ def step_1():
     for a, b, c, d in pairs:
         set_one = set(irange(a, b))
         set_two = set(irange(c, d))
+        # find the difference between the sets
         if len(set_one - set_two) == 0 or len(set_two - set_one) == 0:
             result += 1
 
-    print(result)
+    return result
 
 
-step_1()
+def step_2() -> int:
+    text = read_input()
+    pairs = parse_input(text)
+    result = 0
+
+    for a, b, c, d in pairs:
+        set_one = set(irange(a, b))
+        set_two = set(irange(c, d))
+        # find the intersection between the sets
+        if len(set_one & set_two) > 0:
+            result += 1
+
+    return result
+
+
+print('Step 1', step_1(), sep=': ')
+print('Step 2', step_2(), sep=': ')
