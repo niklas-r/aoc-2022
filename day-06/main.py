75ad83963a7a2f1b15f532c07946230d8cd6ab53
Niklas Kahn <niklas@neatminds.se>
2022-12-07 23:27:07 +0100

    Day 6 step 2

--- day-06/main.py
+++ day-06/main.py
@@ -13,4 +13,12 @@ def step_1():
             return i
 
 
+def step_2():
+    input = list(read_input())
+    for i in range(14, len(input)):
+        if is_unique_occurrences(input[i-14:i]):
+            return i
+
+
 print('Step 1', step_1(), sep=': ')  # 1155
+print('Step 2', step_2(), sep=': ')  # 2789
