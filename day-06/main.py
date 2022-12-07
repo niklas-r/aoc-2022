1a237d06e2b209bd569ea15e831a5d02acba8eaa
Niklas Kahn <niklas@neatminds.se>
2022-12-07 23:22:26 +0100

    Day 6 step 1

---
+++ day-06/main.py
@@ -0,0 +1,16 @@
+def read_input():
+    with open('./day-06/input/input.txt') as f:
+        return f.read()
+
+
+def is_unique_occurrences(x): return len(set(x)) == len(x)
+
+
+def step_1():
+    input = list(read_input())
+    for i in range(4, len(input)):
+        if is_unique_occurrences(input[i-4:i]):
+            return i
+
+
+print('Step 1', step_1(), sep=': ')  # 1155
